8b1109af70047e97bdea6504bc2edbff7c06ebdc
AyushUnleashed <myzealouslifeofficial@gmail.com>
2022-09-02 20:50:07 +0530

    Time moved out of bubble

--- app/src/main/java/com/ayushunleashed/mitram/utils/DateClass.kt
+++ app/src/main/java/com/ayushunleashed/mitram/utils/DateClass.kt
@@ -1,15 +1,21 @@
 package com.ayushunleashed.mitram.utils
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import com.google.android.material.timepicker.TimeFormat
 import java.sql.Time
 import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class DateClass() {
     lateinit var date: Date
     lateinit var currentDate: String
     lateinit var currentTime: String
+    lateinit var currentTimeStamp:String
 
     public fun getTimeAndDate(): String {
         date= Calendar.getInstance().time
@@ -22,5 +28,15 @@ class DateClass() {
         date= Calendar.getInstance().time
         currentTime =DateFormat.getTimeInstance().format(date.time)
         return currentTime
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    public fun getTimeStamp():String{
+        val current = LocalDateTime.now()
+
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val formatted = current.format(formatter)
+        currentTimeStamp = formatted;
+        return currentTimeStamp;
     }
 }
