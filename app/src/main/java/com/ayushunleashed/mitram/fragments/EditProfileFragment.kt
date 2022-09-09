7f4cefd42717bd24fdbb73a2e3478e26c1ba7a83
AyushUnleashed <myzealouslifeofficial@gmail.com>
2022-09-10 01:07:45 +0530

    Fixed empty lines at end of bio and name

--- app/src/main/java/com/ayushunleashed/mitram/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/ayushunleashed/mitram/fragments/EditProfileFragment.kt
@@ -180,8 +180,12 @@ class EditProfileFragment : Fragment() {
 
     fun saveDataToDB(){
 
-        currentUserModel.displayName =  binding.etvUserNameEditProfile.text.toString()
-        currentUserModel.bio = binding.etvUserBioEditBio.text.toString()
+        var userName = binding.etvUserNameEditProfile.text.toString()
+        userName = userName.replace("(^[\\r\\n]+|[\\r\\n]+$)".toRegex(), "");
+        var userBio = binding.etvUserBioEditBio.text.toString()
+        userBio = userBio.replace("(^[\\r\\n]+|[\\r\\n]+$)".toRegex(), "");
+        currentUserModel.displayName =  userName
+        currentUserModel.bio = userBio
 
         Log.d("GENERAL",currentUserModel.toString())
 
