15579cc26542d7e14d1c5a7ccd2851b3fa1be65b
AyushUnleashed <myzealouslifeofficial@gmail.com>
2022-11-11 22:26:42 +0530

    Added Create Account UI Page

--- app/src/main/java/com/ayushunleashed/mitram/activities/LogInEmail.kt
+++ app/src/main/java/com/ayushunleashed/mitram/activities/LogInEmail.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ayushunleashed.mitram.R
 
-class SignUpEmail : AppCompatActivity() {
+class LogInEmail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_email)
